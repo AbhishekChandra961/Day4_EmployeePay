761ccf0494f601298fadea83c46ca33a4f6de476
Abhishek <achandra961@gmail.com>
2023-04-13 09:02:54 +0530

    UC1

---
+++ src/main/java/com/employeepay/demo/EmployeePayApplication.java
@@ -0,0 +1,14 @@
+package com.employeepay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeepay/demo/EmployeePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeepay.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
